bce9e10220784110d9c1ba5b5789a284076a3e49
thomasegles <71641615+thomasegles@users.noreply.github.com>
2020-09-21 15:50:07 +0200

    inital commit

---
+++ hue1/PrimeSieve/src/primesive/PrimeSieve.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primesive;
+
+/**
+ *
+ * @author Thomas
+ */
+public interface PrimeSieve {
+
+    public boolean isPrime(int p);
+
+    public void printPrimes();
+}
